a9900e021edab341026e423289bfa6629950505c
joeunbyula <gkdldy1109@gmail.com>
2023-12-04 16:14:23 +0900

    Outlet, createGlobalStyle 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,52 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Layout from "./components/layout.tsx";
+import Home from "./routes/home.tsx";
+import Profile from "./routes/profile.tsx";
+import Login from "./routes/login.tsx";
+import CreateAccount from "./routes/create-account.tsx";
+import {createGlobalStyle} from "styled-components";
+import reset from "styled-reset";
+
+const router = createBrowserRouter([
+  {
+    path:"/"
+    , element: <Layout/>
+    , children: [
+      {
+        path: ""
+        , element: <Home/>
+      },
+      {
+        path: "profile"
+        , element: <Profile/>
+      }
+    ]
+  },
+  {
+    path: "/login"
+    , element: <Login/>
+  },
+  {
+    path: "/create-account"
+    , element: <CreateAccount/>
+  }
+]);
+
+const GlobalStypes=createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color: whitesmoke;
+  }
+`
 function App() {
   return (
     <>
+      <GlobalStypes/>
+      <RouterProvider router={router}/>
     </>
   )
 }
---
+++ src/routes/create-account.tsx
@@ -0,0 +1,4 @@
+export default function CreateAccount() {
+    return <h1>create accout</h1>;
+
+}
