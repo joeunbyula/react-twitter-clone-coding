64055a1ab0ac0479c391ab66dedcb0d18bee6377
joeunbyula <gkdldy1109@gmail.com>
2023-12-05 15:19:42 +0900

    firebase, post tweet

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,8 +1,13 @@
-import {auth} from "../firebase.tsx";
+import PostTweetForm from "../components/post-tweet-form.tsx";
+import styled from "styled-components";
 
+const Wrapper = styled.div`
+
+`;
 export default function Home() {
-    const logOut = () => {
-        auth.signOut();
-    }
-    return <h1><button onClick={logOut}>LogOut</button> </h1>
+    return (
+        <Wrapper>
+            <PostTweetForm/>
+        </Wrapper>
+    );
 }
